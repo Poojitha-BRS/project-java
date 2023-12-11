5789763b8af58b9b4000a36dcb5c85b33a7ac19d
PoojithaKarnela <karnelapoojitha@gmail.com>
2023-12-11 11:51:44 +0530

    commiting

---
+++ Sample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Sample{
+    public static void main(String args[])
+    {
+        int i,n=10;
+        for(i=0;i<n;i++)
+        {
+           System.out.println("num: "+i);
+        }
+    }
+}
